c1f94c5bc0829f5bba9bf23d8380052ea029973e
Julie Ores <julie.ores@gmail.com>
2018-10-18 14:00:47 -0500

    server updates

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParsar = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./route");
+const app = express();
+const PORT = process.env.PORT || 27017
+
+app.use(body.bodyParsar.urlencoded({ extended: false}));
+app.use(bodyParsar.json());
+
+app.use(express.statis("client/build"));
+
+app.use(routes);
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(
+process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+
+    {
+        useMongoClient: true
+
+    }
+);
+
+app.listen(PORT, function(){
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+
+});
